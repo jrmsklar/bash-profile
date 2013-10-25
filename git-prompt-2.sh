49683901e601c4612b2c1bcba39e1707940bcbb8
Josh Sklar <jsklar@detroitlabs.com>
2013-10-25 11:05:24 -0400

    updated git status output in git-prompt*.sh
    used the new git status output (nothing to commit, working directory clean)

--- git-prompt-2.sh
+++ git-prompt-2.sh
@@ -3,7 +3,7 @@ c_git_clean=`tput setaf 2`
 c_git_dirty=`tput setaf 1`
 
 function parse_git_dirty {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo " $c_git_dirty"
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working directory clean" ]] && echo " $c_git_dirty"
 }
 
 function parse_git_branch_name {
--- git-prompt.sh
+++ git-prompt.sh
@@ -3,7 +3,7 @@ c_git_clean=`tput setaf 2`
 c_git_dirty=`tput setaf 1`
 
 function parse_git_dirty {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo " $c_git_dirty***${c_reset}"
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working directory clean" ]] && echo " $c_git_dirty***${c_reset}"
 }
 function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)] /"
